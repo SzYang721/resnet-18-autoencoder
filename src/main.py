c429314c004f88bda531b59c790b3727be5082f9
Songze_Yang <yangsongze2008@gmail.com>
2024-04-18 13:16:43 +1000

    everything

--- src/main.py
+++ src/main.py
@@ -29,9 +29,9 @@ if __name__=='__main__':
     print("Defining model...")
     cae = AE('default')
     Encoder = encoder(fc_bias=True,fixdim=True,ETF_fc=True,SOTA=False)
-    load_path = "/data5/model_weights/"+"Resnet18-design-SGD"+"/"
-    i = TARGET_EPOCHS
-    Encoder.load_state_dict(torch.load(load_path + 'epoch_' + str(i + 1).zfill(3) + '.pth',map_location=device))
+    # load_path = "/data5/model_weights/"+"Resnet18-design-SGD"+"/"
+    # i = TARGET_EPOCHS
+    # Encoder.load_state_dict(torch.load(load_path + 'epoch_' + str(i + 1).zfill(3) + '.pth',map_location=device))
     Decoder = decoder(fc_bias=True,fixdim=True,ETF_fc=True,SOTA=False)
     cae = Autoencoder(Encoder, Decoder)
     summary(cae.encoder.to(device), input_size=(3, 32, 32), device = device)
